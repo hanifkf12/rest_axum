fab662366261f475e55fc16806218c64ee4e1529
hanifkf12 <hanifkhoirulfathan@gmail.com>
2025-12-15 20:05:26 +0700

    change handler

--- src/interfaces/http/handlers.rs
+++ src/interfaces/http/handlers.rs
@@ -1,28 +1,22 @@
-use std::sync::Arc;
+use crate::application::post_service::PostService;
+use axum::Json;
 use axum::extract::State;
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
-use axum::Json;
-use crate::application::post_service::PostService;
+use std::sync::Arc;
 
 #[derive(Clone)]
 pub struct AppState {
-    pub post_service: Arc<PostService>
+    pub post_service: Arc<PostService>,
 }
 
 pub async fn health_check() -> impl IntoResponse {
-    (StatusCode::OK, Json(serde_json::json!({
-        "status": "ok",
-        "message": "Server is running"
-    })))
+    (StatusCode::OK, Json(serde_json::json!({"status": "ok"}))).into_response()
 }
 
-pub async fn list_posts(
-    State(state): State<AppState>,
-) -> impl IntoResponse {
-    match state.post_service.get_all().await { 
+pub async fn list_posts(State(state): State<AppState>) -> impl IntoResponse {
+    match state.post_service.get_all().await {
         Ok(posts) => (StatusCode::OK, Json(serde_json::json!(posts))).into_response(),
-        Err(_) => StatusCode::INTERNAL_SERVER_ERROR.into_response()
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
     }
 }
-
